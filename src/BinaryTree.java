133b630e03c86561e58980d8d44bdd6f9c49e746
Cooper Anderson <cooper@cooperstuff.ninja>
2019-01-25 11:35:11 -0500

    Add Preorder constructor

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,8 +1,7 @@
 
-
 /**
  * 
- * @author Matt Boutell and <<<YOUR NAME HERE>>>.
+ * @author Matt Boutell and Cooper Anderson.
  */
 
 public class BinaryTree {
@@ -26,9 +25,8 @@ public class BinaryTree {
 	 *            L,R, 2, or 0.
 	 */
 	public BinaryTree(String chars, String children) {
-		// TODO: Implement this constructor. You may not add any other fields to
-		// the BinaryTree class, but you may add local variables and helper
-		// methods if you like.
+		if (chars.length() != 0) this.root = new BinaryNode(chars.charAt(0), chars, children, new IntWrapper());
+		else this.root = NULL_NODE;
 	}
 
 	/**
@@ -47,6 +45,10 @@ public class BinaryTree {
 		return root.toStructuredString();
 	}
 
+	public class IntWrapper {
+		int index;
+	}
+
 	// /////////////// BinaryNode
 	public class BinaryNode {
 
@@ -58,6 +60,18 @@ public class BinaryTree {
 			this.data = element;
 			this.left = NULL_NODE;
 			this.right = NULL_NODE;
+		}
+
+		public BinaryNode(Character element, String chars, String children, IntWrapper index) {
+			this.data = element;
+			char c = children.charAt(index.index);
+			if (c == 'L' || c == '2' && index.index + 1 < children.length()) this.left = new BinaryNode(chars.charAt(++index.index), chars, children, index);
+			else this.left = NULL_NODE;
+			if (c == 'R' || c == '2' && index.index + 1 < children.length()) this.right = new BinaryNode(chars.charAt(++index.index), chars, children, index);
+			else this.right = NULL_NODE;
+		}
+
+		public void construct(String chars, String children, IntWrapper index) {
 		}
 
 		@Override
